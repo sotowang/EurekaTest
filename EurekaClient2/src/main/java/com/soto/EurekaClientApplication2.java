6e4111e979fd4f046b473616251422207f785f09
sotowang <sotowang@qq.com>
2019-07-10 09:12:14 +0800

    @Import导入positive类

--- EurekaClient2/src/main/java/com/soto/EurekaClientApplication2.java
+++ EurekaClient2/src/main/java/com/soto/EurekaClientApplication2.java
@@ -1,11 +1,17 @@
 package com.soto;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+//@SpringBootApplication
 @EnableDiscoveryClient
+@Configuration
+@EnableAutoConfiguration
+//@Import({})
 public class EurekaClientApplication2 {
 
     public static void main(String[] args) {
